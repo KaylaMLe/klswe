09812e1598d6fbc1d9e8ae13cbd74f703a8edaca
KaylaMLe <kaylale2014@gmail.com>
2024-02-05 20:33:55 -0800

    added animation and sfx to flowers

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,11 @@
 import { ReactElement, useState } from 'react';
 import { css } from '@emotion/css';
+import chime from './assets/audio/chime.mp3';
 import flower from './assets/images/flower.png';
 import tree from './assets/images/tree.png';
+import voloff from './assets/images/voloff.png';
+import volon from './assets/images/volon.png';
+import { keyframes } from '@emotion/react';
 
 
 interface FlowerProps {
@@ -12,6 +16,7 @@ interface FlowerProps {
 };
 
 export function Home(): ReactElement {
+	const [muted, setMuted] = useState(false);
 	const homeStyle: string = css({
 		boxSizing: 'inherit',
 		height: '90vh',
@@ -19,28 +24,55 @@ export function Home(): ReactElement {
 		display: 'flex',
 		alignItems: 'center',
 		justifyContent: 'center',
+		position: 'relative',
 	});
 
 	return (
 		<div className={homeStyle}>
-			<Tree />
+			<MuteBtn onClick={() => { setMuted(!muted) }} muted={muted} />
+			<Tree muted={muted} />
 		</div>
 	);
 }
 
-function Tree(): ReactElement {
+function MuteBtn({ onClick, muted }: { onClick: () => void, muted: boolean }): ReactElement {
+	const muteBtnStyle = css({
+		cursor: 'pointer',
+		position: 'absolute',
+		top: '5px',
+		left: '5px',
+		width: '200px',
+		fontSize: '11pt',
+		backgroundColor: '#EEA8D9',
+		borderRadius: '2rem',
+		borderStyle: 'solid',
+		borderColor: '#602267',
+		padding: '8px',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	});
+
+	return (
+		<button className={muteBtnStyle} onClick={onClick}>
+			<img src={muted ? volon : voloff} alt='Mute all SFX' />
+			{muted ? 'Unmute sound effects' : 'Mute sound effects'}
+		</button>
+	);
+}
+
+function Tree({ muted }: { muted: boolean }): ReactElement {
 	const [flowerProps, setFlowerProps] = useState<FlowerProps[]>([]);
 	const [flowerId, setFlowerId] = useState(0);
 
 	const treeBoxStyle: string = css({
+		cursor: 'pointer',
 		position: 'relative',
 	});
 
 	const addFlower = () => {
-		if (flowerProps.length === 50) {
-			const f = flowerProps;
-			f.shift();
-			setFlowerProps(f);
+		if (flowerProps.length === 64) {
+			setFlowerProps(flowerProps.slice(1));
 		}
 
 		setFlowerProps([...flowerProps,
@@ -53,11 +85,23 @@ function Tree(): ReactElement {
 			rotation: Math.floor(Math.random() * 360),
 		}]);
 
-		setFlowerId((flowerId + 1) % 50);
+		setFlowerId((flowerId + 1) % 64);
+
+		if (!muted) {
+			const audioElement = document.getElementById('clickSound') as HTMLAudioElement;
+
+			if (audioElement) {
+				audioElement.play();
+			}
+		}
 	};
 
 	return (
 		<div className={treeBoxStyle} onClick={addFlower}>
+			<audio id="clickSound">
+				<source src={chime}></source>
+				Your browser does not support the audio element.
+			</audio>
 			<img src={tree} alt='A drawing of a bonsai tree' />
 			{flowerProps.map(props => (
 				<Flower key={props.id} id={props.id} dX={props.dX} dY={props.dY} rotation={props.rotation} />
@@ -68,11 +112,23 @@ function Tree(): ReactElement {
 
 function Flower({ id, dX, dY, rotation }
 	: { id: number, dX: number, dY: number, rotation: number }): ReactElement {
+	const spinAnimation = keyframes`
+		from {
+			rotate: 0deg;
+		}
+		to {
+			rotate: ${rotation}deg;
+		}
+	`;
+
 	const flowerStyle: string = css({
 		position: 'absolute',
 		left: `${dX}px`,
 		top: `${dY}px`,
 		rotate: `${rotation}deg`,
+		'@media(prefers-reduced-motion: no-preference)': {
+			animation: `${spinAnimation} 0.5s ease-in-out`
+		},
 	});
 
 	return (
