e9a4c1a7e62dfab22d700a54333c877c997577f7
KaylaMLe <kaylale2014@gmail.com>
2024-03-03 13:31:58 -0800

    separated dropdown components from navbar components

---
+++ src/DropDownMenu.tsx
@@ -0,0 +1,86 @@
+import { css } from '@emotion/css';
+import React, { ReactNode, useState } from 'react';
+import ddClosedDark from './assets/images/dd-closed-dark.png';
+import ddClosedLight from './assets/images/dd-closed-light.png';
+import dropdownOpen from './assets/images/dropdown-open.png';
+import externalLink from './assets/images/external-link.png';
+
+export function DropDownMenu({ label, children }:
+	{ label: string, children: ReactNode }): React.JSX.Element {
+	const [hovered, setHovered] = useState(false);
+	const [expanded, setExpanded] = useState(false);
+
+	const ddContainer = css({
+		position: 'relative',
+	});
+
+	const ddBtn = css({
+		backgroundColor: expanded ? '#000080' : '#A3A3FF',
+		color: expanded ? '#A3A3FF' : '#000080',
+		borderColor: '#000080',
+		borderRadius: '0.5rem',
+		borderStyle: 'solid',
+		fontSize: '12pt',
+		marginLeft: '0.1rem',
+		padding: '0.75rem',
+		'@media(prefers-reduced-motion: no-preference)': {
+			transition: 'background-color 0.5s ease, color 0.5s ease',
+		},
+		':hover': {
+			backgroundColor: '#000080',
+			color: '#A3A3FF',
+		},
+	});
+
+	const ddContent = css({
+		backgroundColor: '#000080',
+		borderRadius: '0.5rem',
+		padding: '0.75rem',
+		width: '15vw',
+		minWidth: '128px',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		position: 'absolute',
+		top: '80%',// overlays content on top of drop down button
+		right: '0',
+		zIndex: '1',
+	});
+
+	return (
+		<div className={ddContainer}>
+			<button
+				className={ddBtn}
+				onClick={() => { setExpanded(!expanded) }}
+				onMouseEnter={() => { setHovered(true) }}
+				onMouseLeave={() => { setHovered(false) }}
+			>
+				{label}
+				<img
+					src={expanded ? dropdownOpen : hovered ? ddClosedLight : ddClosedDark}
+					alt={expanded ? 'open dropdown menu' : 'closed dropdown menu'}
+				/>
+			</button>
+			{expanded &&
+				<div className={ddContent}>
+					{children}
+				</div>
+			}
+		</div>
+	);
+}
+
+export function ExternalLink({ text, link }: { text: string, link: string }): React.JSX.Element {
+	const externalLinkStyle = css(
+		{
+			color: '#A3A3FF',
+		}
+	);
+
+	return (
+		<a className={externalLinkStyle} href={link}>
+			{text}
+			<img src={externalLink} alt='external link' />
+		</a>
+	);
+}
--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,12 +1,9 @@
 import { css } from '@emotion/css';
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { usePageNumber } from './pages/PageNumberContext';
 import { HOME, ABOUT_ME } from './pages/PageNumbers';
-import ddClosedDark from './assets/images/dd-closed-dark.png';
-import ddClosedLight from './assets/images/dd-closed-light.png';
-import dropdownOpen from './assets/images/dropdown-open.png';
-import externalLink from './assets/images/external-link.png';
+import { DropDownMenu, ExternalLink } from './DropDownMenu';
 
 export function NavBar(): React.JSX.Element {
 	const navBarStyle = css({
@@ -121,84 +118,5 @@ function NavBtn({ label, link, targetPage }:
 		>
 			{label}
 		</button >
-	);
-}
-
-function DropDownMenu({ label, children }: { label: string, children: ReactNode }): React.JSX.Element {
-	const [hovered, setHovered] = useState(false);
-	const [expanded, setExpanded] = useState(false);
-
-	const ddContainer = css({
-		position: 'relative',
-	});
-
-	const ddBtn = css({
-		backgroundColor: expanded ? '#000080' : '#A3A3FF',
-		color: expanded ? '#A3A3FF' : '#000080',
-		borderColor: '#000080',
-		borderRadius: '0.5rem',
-		borderStyle: 'solid',
-		fontSize: '12pt',
-		marginLeft: '0.1rem',
-		padding: '0.75rem',
-		'@media(prefers-reduced-motion: no-preference)': {
-			transition: 'background-color 0.5s ease, color 0.5s ease',
-		},
-		':hover': {
-			backgroundColor: '#000080',
-			color: '#A3A3FF',
-		},
-	});
-
-	const ddContent = css({
-		backgroundColor: '#000080',
-		borderRadius: '0.5rem',
-		padding: '0.75rem',
-		width: '15vw',
-		minWidth: '128px',
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-		position: 'absolute',
-		top: '80%',// overlays content on top of drop down button
-		right: '0',
-		zIndex: '1',
-	});
-
-	return (
-		<div className={ddContainer}>
-			<button
-				className={ddBtn}
-				onClick={() => { setExpanded(!expanded) }}
-				onMouseEnter={() => { setHovered(true) }}
-				onMouseLeave={() => { setHovered(false) }}
-			>
-				{label}
-				<img
-					src={expanded ? dropdownOpen : hovered ? ddClosedLight : ddClosedDark}
-					alt={expanded ? 'open dropdown menu' : 'closed dropdown menu'}
-				/>
-			</button>
-			{expanded &&
-				<div className={ddContent}>
-					{children}
-				</div>
-			}
-		</div>
-	);
-}
-
-function ExternalLink({ text, link }: { text: string, link: string }): React.JSX.Element {
-	const externalLinkStyle = css(
-		{
-			color: '#A3A3FF',
-		}
-	);
-
-	return (
-		<a className={externalLinkStyle} href={link}>
-			{text}
-			<img src={externalLink} alt='external link' />
-		</a>
 	);
 }
