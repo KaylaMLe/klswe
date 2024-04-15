44db17f853bbbbf4934b1c6a16bfdd15391098cd
KaylaMLe <kaylale2014@gmail.com>
2024-04-14 20:43:44 -0700

    adjusted 404 page styling

--- src/components/Pages/NotFoundError.tsx
+++ src/components/Pages/NotFoundError.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { css } from '@emotion/css';
 import { NOT_FOUND_ERROR } from '../../hooks/PageNumbers';
 import { Page } from './Page';
 
 export default function NotFoundError(): React.JSX.Element {
+	const errorStyle = css({
+		color: 'white',
+	});
+
+	const linkStyle = css({
+		color: '#006DFF',
+		':visited': {
+			color: '#9B4BF7',
+		},
+	});
+
 	return (
 		<Page pageNumber={NOT_FOUND_ERROR.pageNumber} title='Not Found'>
-			<h2>404: Page Not Found</h2>
-			<a href='/'>Click here to go to the home page.</a>
+			<h2 className={errorStyle}>404: Page Not Found</h2>
+			<a className={linkStyle} href='/'>Click here to go to the home page.</a>
 		</Page>
 	);
 }
