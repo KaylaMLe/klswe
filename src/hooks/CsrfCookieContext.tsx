840b6f83406fb9945ec239adca07a10c967844a3
KaylaMLe <kaylale2014@gmail.com>
2024-09-03 20:55:00 -0700

    created context for streamlined csrf handling

---
+++ src/hooks/CsrfCookieContext.tsx
@@ -0,0 +1,41 @@
+import { ContextProviderProps } from './ContextProviderProps';
+import Cookies from 'js-cookie';
+import React, { createContext, useEffect, useState } from 'react';
+
+interface CsrfCookieContextProps {
+	csrfCookie: string | null;
+}
+
+export const CsrfCookieContext = createContext<CsrfCookieContextProps>({
+	csrfCookie: null,
+});
+
+export const CsrfCookieProvider: React.FC<ContextProviderProps> = ({ children }) => {
+	const [csrfCookie, setCsrfCookie] = useState<string | null>(null);
+
+	useEffect(() => {
+		const fetchCsrfToken = async () => {
+			const storedCsrfCookie = Cookies.get('csrftoken') || localStorage.getItem('csrftoken');
+
+			if (storedCsrfCookie) {
+				setCsrfCookie(storedCsrfCookie);
+			} else {
+				await fetch('https://api.klswe.com/csrf-setter/', {
+					method: 'GET',
+					credentials: 'include',
+				});
+
+				const fetchedCsrfCookie = Cookies.get('csrftoken') || null;
+				setCsrfCookie(fetchedCsrfCookie);
+			}
+		}
+
+		fetchCsrfToken();
+	}, []);
+
+	return (
+		<CsrfCookieContext.Provider value={{ csrfCookie }}>
+			{children}
+		</CsrfCookieContext.Provider>
+	);
+};
