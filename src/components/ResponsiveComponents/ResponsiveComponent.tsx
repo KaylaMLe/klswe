b842173c056b9af8fac1c665e72ff2849c244a72
KaylaMLe <kaylale2014@gmail.com>
2024-09-25 14:23:45 -0700

    condensed responsive component logic

--- src/components/ResponsiveComponents/ResponsiveComponent.tsx
+++ src/components/ResponsiveComponents/ResponsiveComponent.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { css, CSSObject } from '@emotion/css';
 import { ResponsiveProps } from '../../types/ResponsiveComponentTypes';
+import { useCurrentPage } from '../../hooks/PageNumberContext';
+import { HOME } from '../../hooks/PageNumbers';
 import { useIsMobile } from '../../hooks/ViewPortContext';
 
-export function ResponsiveComponent({
-	Component,
-	styles,
-	children,
-	...props
-}: React.PropsWithChildren<ResponsiveProps>): React.JSX.Element {
+export function ResponsiveComponent({ Component, styles, children, ...props }
+	: React.PropsWithChildren<ResponsiveProps>): React.JSX.Element {
 	const { isMobile } = useIsMobile();
+	const { currentPage } = useCurrentPage();
 
 	const style: CSSObject = isMobile ? { ...styles.default, ...styles.alternate }
-		: styles.default;
+		: styles.home && currentPage === HOME.pageNumber ? { ...styles.default, ...styles.home }
+			: styles.default;
 
 	return (
 		<Component className={css(style)} {...props}>
--- src/types/StyleTypes.ts
+++ src/types/StyleTypes.ts
@@ -3,6 +3,7 @@ import { CSSObject } from '@emotion/css';
 export interface DynamicStyles {
 	default: CSSObject,
 	alternate: CSSObject,
+	home?: CSSObject,
 };
 
 export interface ResponsiveNavStyles extends DynamicStyles {
