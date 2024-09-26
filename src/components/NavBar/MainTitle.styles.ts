a74fabd59f4ed055dfb047997c44915efaa40507
KaylaMLe <kaylale2014@gmail.com>
2024-09-25 23:50:09 -0700

    separated main title styles

--- src/components/NavBar/MainTitle.styles.ts
+++ src/components/NavBar/MainTitle.styles.ts
@@ -1,4 +1,33 @@
+import { CSSObject } from "@emotion/css";
 import { DynamicStyles } from "../../types/StyleTypes";
+
+export const headlineStyle: CSSObject = {
+	fontSize: '3rem',
+	lineHeight: '1',
+	margin: '0',
+	boxSizing: 'border-box',
+	paddingBottom: '1rem',
+};
+
+export const linkStyles: DynamicStyles = {
+	default: {
+		textDecoration: 'none',
+		color: '#2FC5CC',
+		fontSize: '2rem',
+		transition: 'font-size 0.5s ease-in-out',
+	},
+	alternate: {},
+	home: {
+		fontSize: 'clamp(4rem, 6rem, 11vmin)',
+	},
+};
+
+export const nameStyle: CSSObject = {
+	lineHeight: '1',
+	margin: '0',
+	boxSizing: 'border-box',
+	paddingBottom: '1rem',
+};
 
 export const titleStyles: DynamicStyles = {
 	default: {
@@ -13,18 +42,5 @@ export const titleStyles: DynamicStyles = {
 		display: 'flex',
 		flexDirection: 'column',
 		width: '100%',
-	},
-};
-
-export const linkStyles: DynamicStyles = {
-	default: {
-		textDecoration: 'none',
-		color: '#2FC5CC',
-		fontSize: '2rem',
-		transition: 'font-size 0.5s ease-in-out',
-	},
-	alternate: {},
-	home: {
-		fontSize: 'clamp(4rem, 6rem, 11vmin)',
 	},
 };
--- src/components/NavBar/MainTitle.tsx
+++ src/components/NavBar/MainTitle.tsx
@@ -4,34 +4,21 @@ import { Link } from 'react-router-dom';
 import { useCurrentPage } from '../../hooks/PageNumberContext';
 import { HOME } from '../../hooks/PageNumbers';
 import { Responsive } from '../ResponsiveComponents/ResponsiveComponent';
-import { titleStyles, linkStyles } from './MainTitle.styles';
+import { headlineStyle, linkStyles, nameStyle, titleStyles } from './MainTitle.styles';
 
 export function MainTitle(): React.JSX.Element {
 	const { currentPage } = useCurrentPage();
 
-	const nameStyle = css({
-		lineHeight: '1',
-		margin: '0',
-		boxSizing: 'border-box',
-		paddingBottom: '1rem',
-	});
-
-	const headlineStyle = css({
-		fontSize: '3rem',
-		lineHeight: '1',
-		margin: '0',
-		boxSizing: 'border-box',
-		paddingBottom: '1rem',
-	});
-
 	return (
 		<Responsive Component='div' styles={titleStyles}>
 			<Responsive Component={Link} styles={linkStyles} to={HOME.link}>
-				<h1 className={nameStyle}>
+				<h1 className={css(nameStyle)}>
 					Kayla Le
 				</h1>
 			</Responsive>
-			{currentPage === HOME.pageNumber && <h2 className={headlineStyle}>Software Engineer</h2>}
+			{currentPage === HOME.pageNumber &&
+				<h2 className={css(headlineStyle)}>Software Engineer</h2>
+			}
 		</Responsive>
 	);
 }
