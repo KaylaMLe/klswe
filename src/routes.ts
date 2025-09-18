48db3609f3f792c7f4840ad154e980fd0d8ca493
KaylaMLe <kaylale2014@gmail.com>
2025-09-18 16:22:47 -0700

    removed failing tests and routes to not yet redesigned pages

--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,14 @@
 import { lazy } from 'react';
 import {
 	HOME,
-	LOGIN,
-	ABOUT_ME,
 	NOT_FOUND_ERROR
 } from './hooks/PageNumbers';
 
 const Home = lazy(() => import('./components/Pages/Home'));
-const Login = lazy(() => import('./components/Pages/Login'));
-const AboutMe = lazy(() => import('./components/Pages/AboutMe'));
 const NotFoundError = lazy(() => import('./components/Pages/NotFoundError'));
 
 const routes = [
 	{ path: HOME.link, component: Home },
-	{ path: LOGIN.link, component: Login },
-	{ path: ABOUT_ME.link, component: AboutMe },
 	{ path: NOT_FOUND_ERROR.link, component: NotFoundError },
 ];
 
