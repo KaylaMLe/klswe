11a2dc08915318fe49d5af39b95d264ad1e3bbdf
KaylaMLe <kaylale2014@gmail.com>
2025-09-18 16:37:06 -0700

    deleted nonexistent imports

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,28 +2,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useCurrentPage } from '../../hooks/PageNumberContext';
-import { LOGIN, ABOUT_ME, FLEXBOX_FUN, TRANSLATE, PDF_TO_FORM } from '../../hooks/PageNumbers';
+import { FLEXBOX_FUN, TRANSLATE, PDF_TO_FORM } from '../../hooks/PageNumbers';
 import { Responsive } from '../ResponsiveComponents/ResponsiveComponent';
 import { Toggle } from '../ResponsiveComponents/ToggleStyledComponent';
 import { DropDownMenu } from './DropDownMenu';
 import { ExternalLink } from './LinkButtons';
 import { MainTitle } from './MainTitle';
-import {
-	aboutMeBtnStyles,
-	btnRowStyles,
-	flexboxFunBtnStyles,
-	navBarStyles,
-	skipStyle
-} from './NavBar.styles';
+import { aboutMeBtnStyles, btnRowStyles, flexboxFunBtnStyles, navBarStyles, skipStyle } from './NavBar.styles';
 
 export function NavBar(): React.JSX.Element {
 	const { currentPage } = useCurrentPage();
 
 	return (
-		<Responsive Component='nav' styles={navBarStyles} data-testid='nav-bar'>
+		<Responsive Component="nav" styles={navBarStyles} data-testid="nav-bar">
 			<SkipToMain />
 			<MainTitle />
-			<Responsive Component='div' styles={btnRowStyles}>
+			<Responsive Component="div" styles={btnRowStyles}>
 				{/* <Toggle
 					Component={Link}
 					label='Log in'
@@ -31,32 +25,25 @@ export function NavBar(): React.JSX.Element {
 					styles={aboutMeBtnStyles}
 					to={LOGIN.link}
 				/> */}
-				<Toggle
-					Component={Link}
-					label='About me'
-					condition={currentPage === ABOUT_ME.pageNumber}
-					styles={aboutMeBtnStyles}
-					to={ABOUT_ME.link}
-				/>
-				<DropDownMenu label='Projects'>
-					<ExternalLink text='Check out my GitHub profile' link='https://github.com/KaylaMLe' />
+				<DropDownMenu label="Projects">
+					<ExternalLink text="Check out my GitHub profile" link="https://github.com/KaylaMLe" />
 					<Toggle
 						Component={Link}
-						label='Flexbox Fun'
+						label="Flexbox Fun"
 						condition={currentPage === FLEXBOX_FUN.pageNumber}
 						styles={flexboxFunBtnStyles}
 						to={FLEXBOX_FUN.link}
 					/>
 					<Toggle
 						Component={Link}
-						label='Translate JS to TS'
+						label="Translate JS to TS"
 						condition={currentPage === TRANSLATE.pageNumber}
 						styles={flexboxFunBtnStyles}
 						to={TRANSLATE.link}
 					/>
 					<Toggle
 						Component={Link}
-						label='PDF to Form'
+						label="PDF to Form"
 						condition={currentPage === PDF_TO_FORM.pageNumber}
 						styles={flexboxFunBtnStyles}
 						to={PDF_TO_FORM.link}
@@ -71,8 +58,10 @@ function SkipToMain(): React.JSX.Element {
 	return (
 		<a
 			css={skipStyle}
-			href='#main'
-			onClick={() => { document.getElementById('main')?.focus() }}
+			href="#main"
+			onClick={() => {
+				document.getElementById('main')?.focus();
+			}}
 		>
 			Skip to main content
 		</a>
